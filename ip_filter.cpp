91772669b8d416871dd483f98134933057db2be0
george-garnov <40470532+george-garnov@users.noreply.github.com>
2018-07-12 08:56:36 +0300

    Update ip_filter.cpp
    Add 
          filter(1)
          filter(46, 70)
          filter_any(46)

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -5,9 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
-//#include <gtest/gtest.h>
 
-//using namespase std;
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -34,58 +32,53 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-//bool ipCmp(std::vector<std::string> str1, std::vector<std::string> str2)
-//{
-//    for(auto &s : str1) s.insert(0,3-s.length(),'0');
-//    for(auto &s : str2) s.insert(0,3-s.length(),'0');
-//    return str1 > str2;
-//}
+void filterIpList(const std::vector< std::vector<std::string> > & ipList,
+                  std::vector< std::vector<std::string> > & ipFlt, const std::string snum)
+{
+    // copy only positive numbers:
+    auto it = std::copy_if(ipList.begin(), ipList.end(), ipFlt.begin(),
+                [snum](std::vector<std::string> v) { for(auto &s : v) if (s == snum) return true; return false; } );
+    ipFlt.resize(std::distance(ipFlt.begin(),it));  // shrink container to new size
+}
+
+void printIpList(const std::vector< std::vector<std::string> > & ipList)
+{
+    for(std::vector<std::vector<std::string> >::const_iterator ip = ipList.cbegin(); ip != ipList.cend(); ++ip)
+    {
+        for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+        {
+            if (ip_part != ip->cbegin())
+            {
+                std::cout << ".";
+            }
+            std::cout << *ip_part;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
 
 std::function<std::vector<std::string>(std::vector<std::string>)> f =
     [](std::vector<std::string> v) { for(auto &s : v) s.insert(0,3-s.length(),'0'); return v; };
 
-//TEST(myTest, Test1) { 
-//    ASSERT_EQ(100, 6*6+8*8);
-//}
-
-//TEST(myTest, Test2) {
-//    ASSERT_FALSE(true==false);
-//}
-
 int main(int argc, char const *argv[])
 {
-    //testing::InitGoogleTest(&argc, argv);
-    //return RUN_ALL_TESTS();
-    
     try
     {
         std::vector<std::vector<std::string> > ip_pool;
-
+        std::vector<std::vector<std::string> > ip_flt;
+        
         for(std::string line; std::getline(std::cin, line);)
         {
             std::vector<std::string> v = split(line, '\t');
             ip_pool.push_back(split(v.at(0), '.'));
         }
         
+        // reverse lexicographically sort
         sort(ip_pool.begin(), ip_pool.end(),
              [](std::vector<std::string> ip1, std::vector<std::string> ip2){return (f(ip1) > f(ip2));});
 
-        // TODO reverse lexicographically sort
-
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
+        printIpList(ip_pool);
         // 222.173.235.246
         // 222.130.177.64
         // 222.82.198.61
@@ -93,27 +86,41 @@ int main(int argc, char const *argv[])
         // 1.70.44.170
         // 1.29.168.152
         // 1.1.234.8
-
+        
         // TODO filter by first byte and output
         // ip = filter(1)
-
+        ip_flt.resize(ip_pool.size());
+        auto it = std::copy_if(ip_pool.begin(), ip_pool.end(), ip_flt.begin(),
+                [](std::vector<std::string> v) { return (v[0] == "1") ? true : false; } );
+        ip_flt.resize(std::distance(ip_flt.begin(), it));  // shrink container to new size
+        printIpList(ip_flt);
         // 1.231.69.33
         // 1.87.203.225
         // 1.70.44.170
         // 1.29.168.152
         // 1.1.234.8
+        
 
         // TODO filter by first and second bytes and output
         // ip = filter(46, 70)
-
+        ip_flt.resize(ip_pool.size());
+        it = std::copy_if(ip_pool.begin(), ip_pool.end(), ip_flt.begin(),
+                [](std::vector<std::string> v) { return (v[0] == "46" && v[1] == "70") ? true : false; } );
+        ip_flt.resize(std::distance(ip_flt.begin(), it));  // shrink container to new size
+        printIpList(ip_flt);
         // 46.70.225.39
         // 46.70.147.26
         // 46.70.113.73
         // 46.70.29.76
+        
 
         // TODO filter by any byte and output
         // ip = filter_any(46)
-
+        ip_flt.resize(ip_pool.size());
+        it = std::copy_if(ip_pool.begin(), ip_pool.end(), ip_flt.begin(),
+                [](std::vector<std::string> v) {for(auto &s : v) if (s == "46") return true; return false; } );
+        ip_flt.resize(std::distance(ip_flt.begin(), it));  // shrink container to new size
+        printIpList(ip_flt);
         // 186.204.34.46
         // 186.46.222.194
         // 185.46.87.231
